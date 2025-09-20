f67099643361abc1f751a33952f2dae1e52fcf2b
Suraj Sunar <surbhu06@gmail.com>
2025-09-20 22:07:22 +0600

    app: adding env setting

--- task-mgr-fastapi/db.py
+++ task-mgr-fastapi/db.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+load_dotenv()
 
-SQLAlchemy_URL = 'postgresql://surbhu:surbhu@localhost:5432/bookstore'
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLAlchemy_URL)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit= False, autoflush= False, bind=engine)
 Base = declarative_base()
